309fc13e18d02473fb93b9d7391808aaa3a793ed
Nayki52 <nazar2007.09.02@gmail.com>
2026-09-14 14:39:41 +0000

    First commit

---
+++ lab01/hello.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+
+print("Hello, " + name + "!")
+
+cource = input("What course are you studying?")
+
+print("You are studying " + cource + "!")
+
+interested = input("What area of cybersecurity interests you?")
+
+print("You are interested in " + interested + "!")
---
+++ lab01/student.py
@@ -0,0 +1,11 @@
+name = input("Enter your name? ")
+
+print("Name: " + name + "!")
+
+studentNum = input("Enter student number?")
+
+print("Student Number: " + studentNum + "!")
+
+year = input("Enter the year?")
+
+print("Year:" + year + "!")
